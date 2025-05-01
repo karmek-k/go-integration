5af9e48ae1d7499415d1c15a5d4dcb7f5f8dc31c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2025-05-01 21:03:35 +0200

    Print available functions

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/karmek-k/go-integration/functions"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("~~~ Numerical Integration ~~~")
+
+	fmt.Println("Available functions:")
+	printFunctions(functions.GetDefault())
+}
+
+// printFunctions prints a table containing function names and their domains.
+func printFunctions(funcs []functions.Function) {
+	const COLUMN_SIZE = 20
+
+	format := fmt.Sprintf("|%% %vv|%% %vv|\n", COLUMN_SIZE, COLUMN_SIZE)
+	divider := strings.Repeat("-", COLUMN_SIZE)
+
+	fmt.Printf(format, divider, divider)
+	fmt.Printf(format, "Name", "Domain")
+	fmt.Printf(format, divider, divider)
+
+	for _, f := range funcs {
+		fmt.Printf(format, f.Name, f.Domain)
+	}
+
+	fmt.Printf(format, divider, divider)
 }
