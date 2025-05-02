67554bf8b348c136032d49d0bb8f6c0bae9db8e4
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2025-05-02 13:33:41 +0200

    Prompt for values

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/karmek-k/go-integration/functions"
 	"github.com/karmek-k/go-integration/integration"
@@ -9,7 +10,7 @@ import (
 )
 
 func main() {
-	fmt.Println("~~~ Numerical Integration ~~~")
+	fmt.Println("~~~ Numerical Integration ~~~\n")
 
 	// TODO: make usable by the user
 	execute()
@@ -21,18 +22,78 @@ func execute() {
 	funcs := functions.Default()
 	printFunctions(funcs)
 
-	f := funcs[2]
+	f := funcs[promptNumberInRange(0, len(funcs)-1)]
+	a, b := promptIntegrationInterval()
+
+	fmt.Println("~~~~~~~")
 	fmt.Printf("Selected function:\t%v\n", f)
-
-	a := 0.0
-	b := 4.0
-
 	fmt.Printf("Integration interval:\t[%v; %v]\n", a, b)
 
 	integral := integration.TrapezoidalIntegral{Function: f, Cuts: 5000}
 
 	result := integral.Calculate(a, b)
 	fmt.Printf("Result:\n%.8v\n", result)
+}
+
+func promptNumberInRange(min, max int) int {
+	var number int
+
+	prompt := "Function: "
+
+	fmt.Print(prompt)
+	fmt.Scan(&number)
+
+	// if out of bonds
+	for number < min || number > max {
+		fmt.Printf(
+			"Provided number should not be lesser than %v and greater than %v.\n",
+			min, max)
+
+		fmt.Print(prompt)
+		fmt.Scan(&number)
+	}
+
+	return number
+}
+
+func promptIntegrationInterval() (float64, float64) {
+	var numberString, prompt string
+	var a, b float64
+	var err error
+
+	prompt = "Left integration bound: "
+
+	fmt.Print(prompt)
+	fmt.Scan(&numberString)
+
+	a, err = strconv.ParseFloat(numberString, 64)
+
+	for err != nil {
+		fmt.Println("Please provide a decimal number.")
+
+		fmt.Print(prompt)
+		fmt.Scan(&numberString)
+
+		a, err = strconv.ParseFloat(numberString, 64)
+	}
+
+	prompt = "Right integration bound: "
+
+	fmt.Print(prompt)
+	fmt.Scan(&numberString)
+
+	b, err = strconv.ParseFloat(numberString, 64)
+
+	for err != nil {
+		fmt.Println("Please provide a decimal number.")
+
+		fmt.Print(prompt)
+		fmt.Scan(&numberString)
+
+		b, err = strconv.ParseFloat(numberString, 64)
+	}
+
+	return a, b
 }
 
 // printFunctions prints a table containing function names and their domains.
